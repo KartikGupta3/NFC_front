514907b847e4e9ef05aeefd394d96f6c2b2dac11
Kartik Gupta <kkartikgupta30@gmail.com>
2023-10-11 11:26:19 +0530

    Splashscreen added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+
+import React, { useEffect } from 'react';
 import LoginPage from './Components/OpeningPage';
+import { Platform } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 const App = () => {
+  useEffect(()=>{
+    if (Platform.OS === 'android'){
+      SplashScreen.hide();
+    }
+  });
   return <LoginPage />;
 };
 export default App;
--- Components/OpeningPage.tsx
+++ Components/OpeningPage.tsx
@@ -1,12 +1,24 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Image, ScrollView, View} from 'react-native';
+import {TouchableOpacity, Image, ScrollView, View, Text} from 'react-native';
 import tw from 'twrnc';
 const OpeningPage = () => {
   return (
-    <ScrollView style={tw`bg-[#1A87DD]`}>
-      <View style={tw`flex items-center mt-32 justify-center`}>
-        <Image style={tw`w-96 h-96`} source={require('./Images/MainLogo.png')} />
+    <ScrollView style={tw`bg-white`}>
+      <View style={tw`bg-[#1A87DD] h-[125] rounded-b-[100px]`}>
+        <View style={tw`flex items-center mt-28`}>
+          <Image style={tw`w-56 h-56`} source={require('./Images/FullLogo.png')} />
+        </View>
+      </View>
+      <View style={tw`flex items-center mt-24 gap-6`}>
+        <TouchableOpacity style={tw`bg-[#1A87DD] w-72 h-12 rounded-lg`}>
+          <View style={tw`flex items-center mt-3.5`}>
+            <Text style={tw`text-white`}>Create New Account</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={tw`text-[#1A87DD]`}>Already have Account? Log In</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
